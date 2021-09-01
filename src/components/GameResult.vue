fa5cb365faf02f2676323ecede9ff85cf48272dd
Christian Tiedemann <CH.Tiedemann@gmx.de>
2021-09-02 00:19:20 +0200

    Start working on score screen

--- src/components/GameResult.vue
+++ src/components/GameResult.vue
@@ -7,14 +7,50 @@
     <div class="game-result">
       <h1>Results</h1>
     </div>
+
+    <div class="results">
+      <div class="results__carddeck">
+        <div class="results__card results--3">
+          <img src="../assets/medal__bronze.svg" />
+          <h4>Simon</h4>
+          <p>50<br />Points</p>
+        </div>
+        <div class="results__card results--2">
+          <img src="../assets/medal__silver.svg" />
+          <h4>Bernhard</h4>
+          <p>100<br />Points</p>
+        </div>
+        <div class="results__card results--1">
+          <img src="../assets/medal__gold.svg" />
+          <h4>Romina</h4>
+          <p>110<br />Points</p>
+        </div>
+      </div>
+      <ul class="results__list">
+        <li class="results__card">
+          <h2>4. <small>Christian</small></h2>
+          <p>40 Points</p>
+        </li>
+        <li class="results__card">
+          <h2>5. <small>Daniel</small></h2>
+          <p>30 Points</p>
+        </li>
+      </ul>
+      <hr />
+      <h1>Stats</h1>
+    </div>
   </bottom-sheet>
 </template>
 
 <script setup>
+// TODO: find a way to make the bottom-sheet closable but not hide the results row at the same time
+import { watchEffect } from 'vue';
+import createConfetti from 'canvas-confetti';
+
 import BottomSheet from '@/components/BottomSheet.vue';
 
 //#region v-model
-defineProps({
+const props = defineProps({
   modelValue: {
     type: Boolean,
     required: true,
@@ -23,12 +59,129 @@ defineProps({
 const emit = defineEmits(['update:modelValue']);
 const close = () => emit('update:modelValue', false);
 //#endregion
+
+let timerId;
+watchEffect(() => {
+  if (props.modelValue) {
+    timerId = setTimeout(createConfetti, 6000);
+  } else {
+    window.clearTimeout(timerId);
+  }
+});
 </script>
 
 <style lang="scss">
+@import '../theme/variables';
+
 .game-result {
   padding: 1rem;
-  // margin-left: 1rem;
-  // margin-right: 1rem;
+}
+
+.results {
+  margin: 1rem;
+  &__carddeck {
+    display: flex;
+    align-items: center;
+
+    .results__card {
+      transform: rotateY(180deg);
+      transform-style: preserve-3d;
+      animation: flip;
+      animation-timing-function: ease-in-out;
+      animation-duration: 0.8s;
+      animation-iteration-count: 1;
+      animation-delay: var(--delay);
+      animation-fill-mode: forwards;
+      backface-visibility: hidden;
+      -webkit-backface-visibility: hidden;
+
+      @keyframes flip {
+        0% {
+          transform: rotateY(180deg);
+        }
+        100% {
+          transform: rotateY(0);
+        }
+      }
+    }
+  }
+
+  &__card {
+    height: 10rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    flex: 1 1 33.33334%;
+    padding: $--card-padding;
+    border: 1px solid $--card-border-color;
+    border-radius: $--card-border-radius;
+    background: $--background-color-base;
+    text-align: center;
+    box-shadow: $--box-shadow-base;
+    perspective: 1000px;
+
+    &.results--1 {
+      height: 14rem;
+      justify-content: flex-start;
+
+      img {
+        margin-bottom: 2.8rem;
+      }
+    }
+
+    img {
+      height: 3.297875rem;
+      margin-bottom: 1rem;
+    }
+
+    h4 {
+      margin: 0;
+    }
+  }
+
+  &--1 {
+    order: 2;
+    z-index: 9;
+    --delay: 5.5s;
+  }
+  &--2 {
+    order: 1;
+    margin-right: -1rem;
+    --delay: 3s;
+  }
+  &--3 {
+    order: 3;
+    margin-left: -1rem;
+    --delay: 0.5s;
+  }
+
+  &__list {
+    padding: 0;
+
+    li {
+      height: auto;
+      margin-bottom: 1rem;
+      flex-direction: row;
+      // padding: 0.5rem 1rem;
+    }
+    h2 {
+      flex: 1 1 50%;
+      display: flex;
+      margin: 0;
+      justify-content: flex-start;
+      align-items: center;
+      > small {
+        margin-left: 1.5rem;
+        font-size: 1rem;
+      }
+    }
+    p {
+      flex: 1 1 50%;
+      display: flex;
+      justify-content: flex-end;
+      margin: 0;
+    }
+  }
 }
 </style>
