a1632e044ef6c129b2dbce481fc26bfee25ca822
Christian Tiedemann <CH.Tiedemann@gmx.de>
2021-09-02 21:48:22 +0200

    Results component

--- src/components/GameResult.vue
+++ src/components/GameResult.vue
@@ -5,39 +5,12 @@
 
   <bottom-sheet hideDrag fullscreen :modelValue="modelValue" @update:modelValue="close" @close="close">
     <div class="game-result">
-      <h1>Results</h1>
-    </div>
-
-    <div class="results">
-      <div class="results__carddeck">
-        <div class="results__card results--3">
-          <img src="../assets/medal__bronze.svg" />
-          <h4>Simon</h4>
-          <p>50<br />Points</p>
-        </div>
-        <div class="results__card results--2">
-          <img src="../assets/medal__silver.svg" />
-          <h4>Bernhard</h4>
-          <p>100<br />Points</p>
-        </div>
-        <div class="results__card results--1">
-          <img src="../assets/medal__gold.svg" />
-          <h4>Romina</h4>
-          <p>110<br />Points</p>
-        </div>
+      <div class="game-result__container">
+        <h1>Results</h1>
       </div>
-      <ul class="results__list">
-        <li class="results__card">
-          <h2>4. <small>Christian</small></h2>
-          <p>40 Points</p>
-        </li>
-        <li class="results__card">
-          <h2>5. <small>Daniel</small></h2>
-          <p>30 Points</p>
-        </li>
-      </ul>
-      <hr />
-      <h1>Stats</h1>
+      <div class="game-result__container game-result--scroll">
+        <results />
+      </div>
     </div>
   </bottom-sheet>
 </template>
@@ -48,6 +21,7 @@ import { watchEffect } from 'vue';
 import createConfetti from 'canvas-confetti';
 
 import BottomSheet from '@/components/BottomSheet.vue';
+import Results from '@/components/Results.vue';
 
 //#region v-model
 const props = defineProps({
@@ -74,114 +48,20 @@ watchEffect(() => {
 @import '../theme/variables';
 
 .game-result {
-  padding: 1rem;
-}
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 
-.results {
-  margin: 1rem;
-  &__carddeck {
-    display: flex;
-    align-items: center;
-
-    .results__card {
-      transform: rotateY(180deg);
-      transform-style: preserve-3d;
-      animation: flip;
-      animation-timing-function: ease-in-out;
-      animation-duration: 0.8s;
-      animation-iteration-count: 1;
-      animation-delay: var(--delay);
-      animation-fill-mode: forwards;
-      backface-visibility: hidden;
-      -webkit-backface-visibility: hidden;
-
-      @keyframes flip {
-        0% {
-          transform: rotateY(180deg);
-        }
-        100% {
-          transform: rotateY(0);
-        }
-      }
-    }
+  &__container {
+    flex: 0 0 4.5rem;
+    padding: 0 1rem;
+    box-sizing: border-box;
   }
 
-  &__card {
-    height: 10rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    flex: 1 1 33.33334%;
-    padding: $--card-padding;
-    border: 1px solid $--card-border-color;
-    border-radius: $--card-border-radius;
-    background: $--background-color-base;
-    text-align: center;
-    box-shadow: $--box-shadow-base;
-    perspective: 1000px;
-
-    &.results--1 {
-      height: 14rem;
-      justify-content: flex-start;
-
-      img {
-        margin-bottom: 2.8rem;
-      }
-    }
-
-    img {
-      height: 3.297875rem;
-      margin-bottom: 1rem;
-    }
-
-    h4 {
-      margin: 0;
-    }
-  }
-
-  &--1 {
-    order: 2;
-    z-index: 9;
-    --delay: 5.5s;
-  }
-  &--2 {
-    order: 1;
-    margin-right: -1rem;
-    --delay: 3s;
-  }
-  &--3 {
-    order: 3;
-    margin-left: -1rem;
-    --delay: 0.5s;
-  }
-
-  &__list {
-    padding: 0;
-
-    li {
-      height: auto;
-      margin-bottom: 1rem;
-      flex-direction: row;
-      // padding: 0.5rem 1rem;
-    }
-    h2 {
-      flex: 1 1 50%;
-      display: flex;
-      margin: 0;
-      justify-content: flex-start;
-      align-items: center;
-      > small {
-        margin-left: 1.5rem;
-        font-size: 1rem;
-      }
-    }
-    p {
-      flex: 1 1 50%;
-      display: flex;
-      justify-content: flex-end;
-      margin: 0;
-    }
+  &--scroll {
+    flex: 1 0 calc(100% - 4.5rem);
+    padding: 1rem;
+    overflow-y: auto;
   }
 }
 </style>
